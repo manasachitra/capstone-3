c29e91a5cfd686bff58435ff3f033193288af139
manasachitra <21P31A04L0@acet.ac.in>
2023-09-27 21:03:00 +0530

    manasa

---
+++ daniel-jebarson.github.io-master/src/constants/index.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "web page design",
+    desc: "I'm very fast learner i have certified in skillup python&advanced python, now iam a full stack developer trainee in technical hub and also i have certified in java and as in infosys spring.. Seeking more experience in to get certified in IT profession and continuing to develop strategies to grow my coding skills..",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "HTML",
+        color: "green-600",
+      },
+    ],
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109025615442608168/pvz.png",
+    link: "https://pvz-2-api.vercel.app/",
+    code: "https://github.com/code-cracked/plants-vs-zombies-api",
+  },
+  {
+    title: "Web-Chat-App",
+    desc: "An app made with NextJs to help each other communicate fastly and securely. We can also search for the friends with their username.Realtime updates on messages.",
+    tags: [
+      {
+        name: "HTML",
+        color: "red-600",
+      },
+      {
+        name: "CSS",
+        color: "green-600",
+      },
+      {
+        name: "BOOTSTRAP",
+        color: "yellow-600",
+      },
+
+    ],
+    img: "https://user-images.githubusercontent.com/88134306/227576773-06c3a1d2-0a1a-4ab2-8d1f-e06d71408410.png",
+    link: "https://web-chat-app-brown.vercel.app",
+    code: "https://github.com/daniel-jebarson/web-chat-app",
+  },
+  {
+    title: "myportofolio",
+    desc: "This is the simple multi-featured discord bot which is in more than 90+ servers that resembles lords mobile as mini game. It contains the featiures of many bots such as dyno,carl-bot,mee6 etc.It helps to authorize the users, audit the messages and can also be used for other server developments and also for mini-games.  ",
+    tags: [
+      {
+        name: "Python",
+        color: "red-600",
+      },
+      {
+        name: "JAVASCRIPT",
+        color: "green-600",
+      },
+
+    ],
+ 
+    img: "https://cdn.discordapp.com/attachments/1096426516667174994/1109035277814611978/Screenshot_19.png",
+    link: "https://github.com/code-cracked/you-farm",
+    code: "https://github.com/code-cracked/you-farm",
+  },
+];
